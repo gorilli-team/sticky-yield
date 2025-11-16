a3141375d6b712d727e8c9f2c06eaca5bacf18f6
Paolo Antonio Rossi <paolo@luduvigo.com>
2025-11-16 09:12:57 -0300

    hack: backend fixes

--- apps/backend/src/services/apyTracker.ts
+++ apps/backend/src/services/apyTracker.ts
@@ -65,12 +65,12 @@ export const TRACKED_POOLS: PoolConfig[] = [
     pool_address: "0xE4847Cb23dAd9311b9907497EF8B39d00AC1DE14",
     chain: "hyperevm",
   },
-  {
-    description: "Hypurr Pooled USD₮0",
-    url: "https://app.hypurr.fi/markets/pooled/999/0xB8CE59FC3717ada4C02eaDF9682A9e934F625ebb",
-    pool_address: "0xB8CE59FC3717ada4C02eaDF9682A9e934F625ebb",
-    chain: "hyperevm",
-  },
+  // {
+  //   description: "Hypurr Pooled USD₮0",
+  //   url: "https://app.hypurr.fi/markets/pooled/999/0xB8CE59FC3717ada4C02eaDF9682A9e934F625ebb",
+  //   pool_address: "0xB8CE59FC3717ada4C02eaDF9682A9e934F625ebb",
+  //   chain: "hyperevm",
+  // },
 ];
 
 /**
